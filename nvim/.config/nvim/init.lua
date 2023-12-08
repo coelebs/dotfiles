791c0a67b6ccf006a65eb62b3f99df7218a02b93
Vincent Kriek <vincent.kriek@alfen.com>
2023-12-08 12:59:45 +0100

    Add useful stuff

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -96,3 +96,10 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins", opts)
+
+
+vim.filetype.add({
+  pattern = {
+    ['jenkinsfile.*'] = 'groovy',
+  },
+})
